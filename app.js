290c90518652c245705e64b10f33248c7fa34fe7
Rahul Kumar <rahulpalahwat@gmail.com>
2023-01-10 22:47:07 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+// this is to use .env file variables
+require('dotenv').config();
+const port = process.env.PORT;
+
+// import connect to connect with the database
+const connect = require('./db/connect');
+
+// home route 
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "App started successfully!" })
+  })
+
+const start = async() => {
+    try {
+        await connect(process.env.URL);
+        app.listen((port) , () => {
+            console.log(`App is listening at port ${port}`);
+        })
+    } catch (error) {
+        console.log({"error": error.message});
+    }
+}
+
+start();
+
