bf12d69009a6c6f8d69fc7d30154b2ae9a0a3919
Rahul Kumar <rahulpalahwat@gmail.com>
2023-01-20 00:04:04 +0530

    Added schema for student and admin

--- models/Admin.js
+++ models/Admin.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const AdminSchema = new Schema({});
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model("Admin", AdminSchema);
--- models/Student.js
+++ models/Student.js
@@ -1,8 +1,32 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const StudentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  batch: {
+    type: Number,
+  },
+  branch: {
+    type: String,
+  },
+  degree: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+});
 
-})
-
-module.exports = mongoose.model("Students" , StudentSchema);
+module.exports = mongoose.model("Students", StudentSchema);
